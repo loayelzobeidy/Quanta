cf8832782ac90e7a828faccec9098dc225c42f03
loayelzobeidy <loayelzobeidy@gmail.com>
2016-11-13 02:07:37 +0200

    the basic project with the Getschedule half done

---
+++ src/github.com/user/hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+// import "os"
+// import "log"
+ //import "github.com/ramin0/chatbot"
+func main() {
+	// Uncomment the following lines to customize the chatbot
+	// chatbot.WelcomeMessage = "What's your name?"
+	// chatbot.ProcessFunc(chatbotProcess)
+
+	// Use the PORT environment variable
+	fmt.Println("Hello World ")
+}
